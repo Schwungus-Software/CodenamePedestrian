5fd9ee06437b0ecd01a65cfc761055d9207ca6c6
nonk123 <me@nonk.dev>
2024-05-28 06:04:38 +0300

    Prevent cheating

--- src/entities/pedo.cpp
+++ src/entities/pedo.cpp
@@ -21,6 +21,18 @@ bool Pedo::is_active() {
 }
 
 void Pedo::update() {
+    if (pos.x < 0.0f) {
+        pos.x = 0.0f;
+    } else if (pos.x + width >= GetScreenWidth() - 1.0f) {
+        pos.x = GetScreenWidth() - 1.0f - width;
+    }
+
+    if (pos.y < 0.0f) {
+        pos.y = 0.0f;
+    } else if (pos.y + height >= GetScreenHeight() - 1.0f) {
+        pos.y = GetScreenHeight() - 1.0f - height;
+    }
+
     if (safe) {
         apply_velocity();
         return;
