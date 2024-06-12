0ca4b5723eb173ff94903cd663362cb0c4db4658
nonk123 <me@nonk.dev>
2024-06-12 04:57:14 +0300

    Style `defs.hpp`

--- src/defs.hpp
+++ src/defs.hpp
@@ -5,12 +5,15 @@
 #include "raylib.h"
 
 const constexpr int TICK_RATE = 60;
+const constexpr float TICK_DELAY = 1.0f / TICK_RATE;
+
+const constexpr float TOTAL_ZOOM = 4.0f;
+
 const constexpr std::size_t PEDO_COUNT = 20;
+const constexpr float CARS_PER_LANE = 1.2f;
 
-const constexpr float TICK_DELAY = 1.0f / TICK_RATE, TOTAL_ZOOM = 4.0f, CARS_PER_LANE = 1.2f;
-
-const constexpr int SAFE_ZONE = 32, SAFE_ZONE_DISPATCH_MARGIN = 4, LANE_WIDTH = 24,
-                    BREAK_LANE_MIN_WIDTH = 12;
+const constexpr int SAFE_ZONE = 32, SAFE_ZONE_DISPATCH_MARGIN = 4;
+const constexpr int LANE_WIDTH = 24, BREAK_LANE_MIN_WIDTH = 12;
 
 namespace Game {
     inline const float width() {
